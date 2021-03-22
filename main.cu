9601cd4697bc8e5834ea8e73bfc2c035e097334c
casasolaivan <81124825+casasolaivan@users.noreply.github.com>
2021-03-22 16:30:02 -0600

    Add files via upload
    actualizacion

--- main.cu
+++ main.cu
@@ -1,4 +1,4 @@
-#include <cuda.h>
+#include <stdlib.h>
 #include <iostream>
 #include <stdio.h>
 #include <time.h>
@@ -12,13 +12,13 @@
 #define BLOCKDIM_X 32
 #define BLOCKDIM_Y 32
 #define BLOCKDIM BLOCKDIM_X * BLOCKDIM_Y
-#define W 512
-#define H 512
+#define W 64
+#define H 64
 #define N W * H
 #define CHANNEL_NUM 1
 using namespace std;
 
-void insertionSort(int *arr, int n);
+void insertionSort_(int *arr, int n);
 void swap_(int *a, int *b);
 uint8_t* readImage(char* file, int &width, int &height, int bpp);
 void writeImage(char* file, int width, int height, uint8_t *image);
@@ -26,6 +26,9 @@ void compareVectors(int *a, int *b);
 void generateRandom(int *a,int rows, int cols);
 void serial_median_filter3x3();
 void parallel_median_filter3x3();
+void printMAT(int *a);
+void printVEC(int *a,int w);
+void copiarValores(int *a, int *b);
 
 int *h_img, *filtered_img_serial, *filtered_img_par;
 int *d_img, *d_img_res;
@@ -37,18 +40,25 @@ float parallelTimer = 0.0;
 
 #define CUDA_CALL(x) {cudaError_t cuda_error__ = (x); if (cuda_error__) printf("CUDA error: " #x " returned \"%s\"\n", cudaGetErrorString(cuda_error__));}
 
-#define PRINT_FLAT2D(A, WIDTH, HEIGHT)\
-    printf("[\n");\
-    for(int i = 0; i < WIDTH; i++){\
-        printf("[");\
-        for(int j = 0; j < HEIGHT; j++){\
-            printf("%f, ", A[i + j * WIDTH]);\
-        }\
-        printf("]\n");\
-    }\
-    printf("]\n");
-
 #define KERNEL_R 3
+
+__device__ void swap(int *a, int *b){
+    int d = *a;
+    *a = *b;
+    *b = d;
+}
+
+__device__ void insertionSort(int *arr, int n){
+    for(int i = 1; i < n; i++){
+        int j = i - 1;
+        int key = arr[i];
+        while(j >= 0 && arr[j] > key){
+            if(arr[j] > arr[j + 1])
+                swap(&arr[j], &arr[j + 1]);
+            j--;
+        }
+    }
+}
 
 __device__ void sort(int *a, int *b, int *c) {
     int d;
@@ -80,32 +90,33 @@ __global__ void medianFilter3x3(const int *src, int w, int h, int *dst){
     if(x - rHalf > 0 && x + rHalf < w && y - rHalf > 0 && y + rHalf < h){
         for (i = x - rHalf, k = 0; i <= x + rHalf; i++, k++) {
             for (j = y - rHalf, l = 0; j <= y + rHalf; j++, l++) {
-                imgBlock[k + l * r] = src[i + j * w];
+                imgBlock[k*r + l] = src[i* w + j];
             }
         }
+
+        insertionSort(imgBlock,r*r);
+
         //Columns
-        for(int row = 0; row < r; row++){
-            sort(&imgBlock[row * r], &imgBlock[row * r + 1], &imgBlock[row * r + 2]);
-        }
+        //for(int row = 0; row < r; row++){
+        //    sort(&imgBlock[row * r], &imgBlock[row * r + 1], &imgBlock[row * r + 2]);
+        //}
         //Rows
-        for(int col = 0; col < r; col++){
-            sort(&imgBlock[col], &imgBlock[col + r], &imgBlock[col + r * 2]);
-        }
+        //for(int col = 0; col < r; col++){
+         //   sort(&imgBlock[col], &imgBlock[col + r], &imgBlock[col + r * 2]);
+        //}
         //Diagonal
-        sort(&imgBlock[0], &imgBlock[1 + r], &imgBlock[2 + 2 * r]);
+        //sort(&imgBlock[0], &imgBlock[1 + r], &imgBlock[2 + 2 * r]);
 
         //Set median
-        dst[x + y * w] = imgBlock[rHalf + rHalf * r];
+        dst[x* w + y ] = imgBlock[rHalf + rHalf * r];
     }
     else if(x < w && y < h){
-        dst[x + y * w] = src[x + y * w];
+        dst[x*w + y ] = src[x*w + y];
     }
 }
 
 int main() {
 
-    // Reservar memoria en device
-    cudaMalloc((void **)&d_img, size);
     // Reservar memoria en host
     h_img = (int *) malloc(size);
     filtered_img_par = (int *) malloc(size);
@@ -114,6 +125,8 @@ int main() {
     //generar img aleatoria
     generateRandom(h_img,W,H);
 
+    // Reservar memoria en device
+    cudaMalloc((void **)&d_img, size);
     // Transferir datos de host a device
     cudaMemcpy(d_img, h_img, size, cudaMemcpyHostToDevice);
 
@@ -138,7 +151,7 @@ void parallel_median_filter3x3(){
     // Reservar memoria en device
     cudaMalloc((void **)&d_img_res, size);
     dim3 blockSize = dim3(BLOCKDIM_X, BLOCKDIM_Y);
-    dim3 gridSize = dim3((unsigned int) ceil(W / BLOCKDIM_X), (unsigned int) ceil(H / BLOCKDIM_Y));
+    dim3 gridSize = dim3(ceil(W / BLOCKDIM_X)+ 1, ceil(H / BLOCKDIM_Y)+ 1);
 
     // Definir timers
     cudaEvent_t start, stop;
@@ -159,11 +172,11 @@ void parallel_median_filter3x3(){
 
     CUDA_CALL(cudaMemcpy(filtered_img_par, d_img_res, size, cudaMemcpyDeviceToHost));
 
-    printf("Source image: \n");
-    //PRINT_FLAT2D(h_img, W, H);
+    //printf("Source image: \n");
+    //printMAT(h_img);
 
-    printf("Blurred image: \n");
-    //PRINT_FLAT2D(filtered_img_par, W, H);
+    //printf("Blurred image par: \n");
+    //printMAT(filtered_img_par);
 }
 
 
@@ -171,8 +184,7 @@ void serial_median_filter3x3(){
     clock_t start = clock();
     for(int i=0;i<W;i++){
         for(int j=0;j<H;j++){
-            int *box3x3;
-            box3x3=(int *) malloc(9);
+            int box3x3[9];
             if(i>0 && j>0 && i<W-1 && j<H-1){
                 box3x3[0] = h_img[(i-1)*W+(j-1)];
                 box3x3[1] = h_img[(i-1)*W+(j)];
@@ -183,29 +195,32 @@ void serial_median_filter3x3(){
                 box3x3[6] = h_img[(i+1)*W+(j-1)];
                 box3x3[7] = h_img[(i+1)*W+(j)];
                 box3x3[8] = h_img[(i+1)*W+(j+1)];
-                insertionSort(box3x3,9);
+
+                insertionSort_(box3x3,9);
                 int median = box3x3[4];
                 filtered_img_serial[i*W+j]=median;
             }
             else{
                 filtered_img_serial[i*W+j]=h_img[i*W+j];
             }
-            free(box3x3);
+
         }
     }
     clock_t end = clock();
     serialTimer = double (end-start) / double(CLOCKS_PER_SEC);
     cout << "Tiempo en obtener filtro img serial: " << serialTimer << endl;
+    //printf("Blurred image ser: \n");
+    //printMAT(filtered_img_serial);
 }
 
 
 void swap_(int *a, int *b){
-    float d = *a;
+    int d = *a;
     *a = *b;
     *b = d;
 }
 
-void insertionSort(int *arr, int n){
+void insertionSort_(int *arr, int n){
     for(int i = 1; i < n; i++){
         int j = i - 1;
         int key = arr[i];
@@ -232,14 +247,14 @@ void generateRandom(int *a,int rows, int cols){
     // Initialize seed
     srand(time(NULL));
     for(int i=0; i<rows*cols; i++){
-        a[i] = rand() % 255;
+        a[i] = rand() % 256;
     }
 }
-void compareVectors(int *parallel, int *serial){
+void compareVectors(int *a, int *b){
     cout<<"Total elements "<<W*H<< endl;
     int diff = 0;
     for(int i= 0; i<W*H; i++)
-        if(parallel[i] != serial[i]){
+        if(a[i] != b[i]){
             diff++;
         }
 
@@ -248,4 +263,29 @@ void compareVectors(int *parallel, int *serial){
     }
     else
         cout << "Vectors are equal!..." << endl;
+}
+
+void printMAT(int *a){
+    cout<<"[\n"<<endl;
+    for(int i = 0; i < W; i++){
+        cout<<"["<<"";
+        for(int j = 0; j < H; j++){
+            cout<<a[i* W + j]<<" ";
+        }
+        cout<<"]\n"<<endl;
+    }
+    cout<<"]\n"<<endl;
+}
+void printVEC(int *a,int w){
+    cout<<"[\n"<<endl;
+    for(int i = 0; i < w; i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<"]\n"<<endl;
+}
+
+void copiarValores(int *a, int *b){
+    for(int i=0;i<W*H;i++){
+        b[i]=a[i];
+    }
 }
