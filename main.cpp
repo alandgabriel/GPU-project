f82361d9d513fc8ac57ebdf5a2733c0be6fb1914
casasolaivan <81124825+casasolaivan@users.noreply.github.com>
2021-03-22 21:18:34 -0600

    Add files via upload
    nuevo

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
 #include "OpenCV.h"
 #include "OpenCV.cpp"
 using namespace std;
+void printVEC(int *a,int w);
 
 int main() {
-    OpenCV cv2;
-    cv2.getFrames();
+    OpenCV cv2("/tmp/tmp.whjZ5QMQTf/outputs/0.jpg");
+    //cv2.getFrames();
+    int h = cv2.getRows();
+    int w = cv2.getCols();
+    cout<<"Ancho: "<<w<<endl;
+    cout<<"Alto: "<<h<<endl;
+    int arr[h*w];
+    cv2.getImage(arr);
+
+    cout<<"Matriz: "<<endl;
+    printVEC(arr,w*h);
+    cv2.saveToimg(arr,"/tmp/tmp.whjZ5QMQTf/test.jpg",w,h);
+
     return 0;
+}
+
+void printVEC(int *a,int w){
+    cout<<"["<<"";
+    for(int i = 0; i < w; i++){
+        cout<<a[i]<<", ";
+    }
+    cout<<"]\n"<<endl;
 }
